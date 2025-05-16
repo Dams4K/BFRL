d533fca03064d3838bf53337d4f69ee211458b98
Dams4K <dams4k@users.noreply.github.com>
2025-05-16 14:55:33 +0200

    forgot this

--- db/models.py
+++ db/models.py
@@ -383,8 +383,8 @@ class Guild(Base):
         d = FormatDict({
             "mode": str(score.mode),
             "time": score.time_best,
-            "old_time": old_time,
-            "rank": score.get_rank(),
+            "old_time": old_time/1000,
+            "rank": score.get_rank()/1000,
             "old_rank": old_rank,
             "member": user
         })
